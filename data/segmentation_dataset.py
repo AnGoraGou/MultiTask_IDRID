aa99a8431a0232a410c913ecac413bb26f025cfe
Gouranga Bala <111289643+AnGoraGou@users.noreply.github.com>
2025-07-04 19:54:47 +0530

    Update segmentation_dataset.py

--- data/segmentation_dataset.py
+++ data/segmentation_dataset.py
@@ -45,3 +45,40 @@ class SegmentationDataset(Dataset):
 
         masks_tensor = torch.stack(masks, dim=0)
         return image, masks_tensor
+
+#This will be used to load data from pre-augmented images and masks paired stored 
+class SegmentationDataset_preAug(Dataset):
+    def __init__(self, image_dir, mask_dirs, joint_transform=None):
+        self.image_dir = image_dir
+        self.mask_dirs = mask_dirs
+        self.joint_transform = joint_transform
+        self.image_names = sorted([f for f in os.listdir(image_dir) if f.endswith('.jpg')])
+
+
+    def __len__(self):
+        return len(self.image_names)
+
+
+    def __getitem__(self, idx):
+        image_name = self.image_names[idx]
+        base_name = os.path.splitext(image_name)[0]
+        img_path = os.path.join(self.image_dir, image_name)
+        image = Image.open(img_path).convert('RGB')
+
+        mask_path = img_path.replace('images','masks').replace('jpg','png')
+
+
+        if os.path.exists(mask_path):
+            mask = Image.open(mask_path).convert('L')
+        else:
+            Print(f'Error 505!!! {mask_path} does not exists.')
+
+
+
+        #print(f'Unique: {np.unique(mask)} of {mask_path} /n {img_path}')
+
+        if self.joint_transform:
+            image, mask = self.joint_transform(image, mask)
+
+        #masks_tensor = torch.stack(masks, dim=0)
+        return image, mask
